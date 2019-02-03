f87398d459148dd90537eec6cb364d1d74fbf1f3
elliottprogrammer <bryan@elliottprogrammer.com>
2019-02-02 19:52:17 -0800

    version 0.3.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,60 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+
+module.exports = (env, options) => {
+    const devMode = options.mode === 'development' ? true : false;
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [{
+                        loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    }, {
+                        loader: 'css-loader', options: { sourceMap: true }
+                    }, {
+                        loader: 'postcss-loader', options: { sourceMap: true }
+                    }, {
+                        loader: 'sass-loader', options: { sourceMap: true }
+                    }]
                 }
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+            ]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new CopyWebpackPlugin([
+                { from:'./src/images',to:'images' } 
+            ]),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new CopyWebpackPlugin([
-            {from:'./src/images',to:'images'} 
-        ])
-    ]
+    }
+    
 }
-
-module.exports = config;
